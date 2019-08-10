c5239660f754e78798c273e686cf4825380d4f6d
Jason Yamada-Hanff <jyamada1@gmail.com>
2019-08-10 14:48:44 -0400

    add example of stateful hypothesis testing
    Notice that the state machine being tested (`TransmissionSystem`) is
    not itself a subclass of hypothesis'
    `RuleBasedStateMachine` (`RBSM`). Instead, a separate class
    `AutoFSMTest` is the `RBSM` subclass. It creates an instance of and
    then exercises the `TransmissionSystem`. `AutoFSMTest` also happens to
    be a state machine, but it is used to test a different state machine.

--- hypothesis_auto/fsm.py
+++ hypothesis_auto/fsm.py
@@ -8,18 +8,34 @@ class State(ABC):
         pass
 
     @abstractmethod
-    def next(self, condition):
+    def next(self, condition) -> "State":
         pass
 
 
 class StateMachine(ABC):
-    def __init__(self, initialState: State, initialCondition) -> None:
-        self.state = initialState
-        self.condition = initialCondition
+    def __init__(self, initial_state: State, initial_condition) -> None:
+        self._state = initial_state
+        self.condition = initial_condition
 
-    @abstractmethod
-    def run(self) -> None:
-        self.state.run()
-        while True:
-            self.state.next(self.condition)
-            self.state.run()
+    def run(self, conditions) -> None:
+        for cond in conditions:
+            self.step(cond)
+            if self.stop_predicate():
+                return
+
+    def step(self, condition):
+        self.condition = condition
+        self.state = self._state.next(self.condition)
+
+    @property
+    def state(self):
+        return self._state
+
+    @state.setter
+    def state(self, value: State):
+        self._state = value
+        self._state.run()
+
+    def stop_predicate(self):
+        """State machine will run until this function returns True"""
+        return False
